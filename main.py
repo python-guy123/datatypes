83744a95ab1d0bd9c86508f58ae05848fe3dc662
python-guy123 <vaishnav.sreekiran@gmail.com>
2024-12-02 20:08:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+a = "hi"
+print("The datatype of A is:", type(a))
+b = 6.8
+print("The datatype of B is:", type(b))
+c = 5
+print("The datatype of C is:", type(c))
+d = False
+print("The datatype of D is:", type(d))
